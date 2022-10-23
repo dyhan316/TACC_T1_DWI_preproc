61c3113cbf1f2d3a5eef2f804633ecbe09a0f104
Dong Yeop Han <tg881334@login1.stampede2.tacc.utexas.edu>
2022-10-22 21:28:01 -0500

    before adding major

--- IMP_MAIN_REAL_every_preprocessing/REAL_step4_qsiprep.py
+++ IMP_MAIN_REAL_every_preprocessing/REAL_step4_qsiprep.py
@@ -9,13 +9,22 @@ import pdb
 #args = parser.parse_args()
 
 ######IMPORTANT TO SELECT THIS########
-ntasks_per_node = 10 #node당 몇명을 할지 (have to be optimized so memory error doeens't occur)
-age_list = ['age_8_to_9','age_9_to_10'] #[,'age_10_to_13','age_13_to_16','age_16_to_18','age_18_to_20']
+ntasks_per_node = 48 #node당 몇명을 할지 (have to be optimized so memory error doeens't occur)
+node = "skx" #or "icx"
+
+age_list = ['age_6_to_7', 'age_8_to_9','age_9_to_10','age_10_to_13','age_13_to_16','age_16_to_18','age_18_to_20']
             #['age_4.5_to_5','age_5_to_5.5']
            #below : not done yet
            #,'age_5.5_to_6','age_6_to_7','age_7_to_8','age_8_to_9','age_9_to_10']
            #['age_0_to_1',  'age_1.5_to_2','age_1_to_1.5','age_3.5_to_4','age_4_to_4.5','age_10_to_13',
            #'age_16_to_18','age_2.5_to_3','age_3_to_3.5','age_13_to_16','age_18_to_20','age_2_to_2.5']
+######################################
+if node	== "skx":
+    total_cpu =	96 #count of total cpus
+elif node == "icx":
+    total_cpu =	160
+
+qsiprep_nthreads_per_sub = int(total_cpu/ntasks_per_node)
 
 #######ASSUMPTIONS######
 curr_dir = os.getcwd()
@@ -70,7 +79,7 @@ for age_range in age_list:
         singularity_command = f"singularity run --cleanenv -B {sub_BIDS_dir}/:/data:ro,{sub_save_dir}/:/out,{str(args.supp_dir)}:/freesurfer {sing_img_dir} /data /out participant -w /out/tmp --output_resolution 1.2 --denoise_after_combining --unringing_method mrdegibbs --b0_to_t1w_transform Affine --intramodal_template_transform SyN --do_reconall --fs-license-file /freesurfer/license.txt --skip_bids_validation" #removeed nthreads because we want to use all available
         if args.infant == True : singularity_command = singularity_command + ' --infant' #add infant option of doing infant 
         ###tr this
-        singularity_command = singularity_command + ' --nthreads 9'
+        singularity_command = singularity_command + f' --nthreads {qsiprep_nthreads_per_sub}'
         
         bash_shell.append(singularity_command)
         #print(f"bash_shell {bash_shell}")
@@ -87,7 +96,7 @@ for age_range in age_list:
     os.makedirs(args.log_dir, exist_ok = True)
     for sub_shell in os.listdir(args.shell_dir):
         shell_to_run = os.path.join(args.shell_dir, sub_shell)
-        sub_id = sub_shell.split('-')[-1][:-4]
+        sub_id = sub_shell.split('-')[-1][:-3]
         command_to_add = shell_to_run + f" >> {args.log_dir}/output-${{LAUNCHER_TSK_ID}}_sub-{sub_id}"
         #print(command_to_add)
         with open(f"{args.base_dir}/step4_shell_outputs/qsipreproc_command_list.txt", "a") as f:
